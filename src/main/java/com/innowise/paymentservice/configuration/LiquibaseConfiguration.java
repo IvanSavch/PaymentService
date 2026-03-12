dfe01652d707c43c1bb0d49cb51a32fa07ce6247
Savva <sav.van.ivan3@mail.ru>
2026-03-12 22:58:25 +0300

    PMS-0:   task-1: Created liquibase for mongoDB

---
+++ src/main/java/com/innowise/paymentservice/configuration/LiquibaseConfiguration.java
@@ -0,0 +1,41 @@
+package com.innowise.paymentservice.configuration;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.ext.mongodb.database.MongoConnection;
+import liquibase.ext.mongodb.database.MongoLiquibaseDatabase;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfiguration {
+    @Bean
+    public Liquibase liquibase() {
+
+        MongoDatabase mongoDatabase;
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            mongoDatabase = mongoClient.getDatabase("payment");
+        }
+
+        MongoConnection connection = new MongoConnection();
+        connection.setMongoDatabase(mongoDatabase);
+
+        Database database = new MongoLiquibaseDatabase();
+        database.setConnection(connection);
+
+        return new Liquibase(
+                "db/changelog/db.changelog-master.yaml",
+                new ClassLoaderResourceAccessor(),
+                database);
+
+    }
+    @Bean
+    CommandLineRunner runLiquibase(Liquibase liquibase) {
+        return args -> liquibase.update();
+    }
+}
---
+++ src/main/java/com/innowise/paymentservice/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.innowise.paymentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "payments")
+public class Payment {
+    @Id
+    private String id;
+    @Indexed
+    private String orderId;
+    @Indexed
+    private String userId;
+    private Status status;
+    private LocalDateTime timestamp;
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal paymentAmount;
+    public enum Status {
+        OK, NOT_OK
+    }
+}
